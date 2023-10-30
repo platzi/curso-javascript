fb5ae6571da826202d95bf9841d48d233ab312c7
Estefany Aguilar <tefy.1@live.com>
2023-10-30 14:46:04 -0500

    Add files via upload

---
+++ modulo-primeros-pasos/clase-tipos-de-datos.js
@@ -0,0 +1,35 @@
+// Tipos de datos primitivos
+
+let nombre = 'Tere'
+console.log(typeof nombre)
+nombre = 'Pablo'
+console.log(nombre)
+
+let edad = 25
+console.log(typeof edad)
+
+let esMayorDeEdad = true
+console.log(typeof esMayorDeEdad)
+
+let nulo = null
+console.log(typeof nulo)
+
+let indefinido = undefined
+console.log(typeof indefinido)
+
+let simbolo = Symbol('único')
+console.log(typeof simbolo)
+
+let numeroMuyGrande = 2n
+console.log(typeof numeroMuyGrande)
+
+// Tipos de datos complejos
+
+let objeto = { nombre: 'Teff' }
+console.log(typeof objeto)
+
+let matriz = [1, 2, 3]
+console.log(typeof matriz)
+
+let miFuncion = function() {}
+console.log(typeof miFuncion)
